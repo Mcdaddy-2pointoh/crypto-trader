47113c19cf922649c4bd289b2f5b5d5b100722c1
Mcdaddy-2pointoh <sharvil.dandekar@gmail.com>
2025-06-11 01:24:01 +0530

    Boilerplate setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='crypto_trader',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Crpyto Trading Suite',
+    author='McDaddy-2pointoh',
+    author_email='mcdaddy2pointoh@gmaol.com',
+    url='https://sharvil-dandekar.vercel.app/',
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
